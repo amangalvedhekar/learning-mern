66ffad87a7c20d73c7ee538b4748babaa303ed87
Anirudh <amangalvedhekar@icloud.com>
2018-07-15 17:06:35 -0400

    working registration form using react state

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,144 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      errors: {},
+    };
+
+    // this.onChange = this.onChange.bind(this);
+
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+    };
+
+    const errorName = {
+      name: 'Please enter name',
+    };
+    axios
+      .post('/api/users/register', newUser)
+      .then(user => console.log(user))
+      .catch(err => {
+        this.setState({
+          errors: errorName,
+        });
+        console.log(this.state);
+      });
+
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div>
-        <h1>Register</h1>
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Sign Up</h1>
+                <p className="lead text-center">Create your DevConnector account</p>
+                <form
+                  noValidate
+                  onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className={
+                        classnames('form-control form-control-lg', {
+                          'is-invalid': errors.name
+                        })
+                      }
+                      placeholder="First Name"
+                      name="firstName"
+                      value={this.state.firstName}
+                      onChange={this.onChange}
+                    />
+                    {errors.name &&
+                    (
+                      <div
+                        className="invalid-feedback">
+                        {errors.name}
+                      </div>
+                    )
+                    }
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Last Name"
+                      name="lastName"
+                      value={this.state.lastName}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="Email Address"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.onChange}
+                    />
+                    <small
+                      className="form-text text-muted">
+                      This site uses Gravatar so if you want a profile image, use a Gravatar email
+                    </small>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Confirm Password"
+                      name="confirmPassword"
+                      value={this.state.confirmPassword}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
