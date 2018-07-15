55bdf21e7c0d310b044a8144d579d110d79c33e3
Anirudh <amangalvedhekar@icloud.com>
2018-07-15 18:53:59 -0400

    setup actions for login

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
-import {GET_ERRORS} from './';
+import setAuthToken from '../utils/setAuthToken';
+import {
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from './';
+import jwt_decode from 'jwt-decode';
 
-export const registerUser = (userData, history) => dispatch =>{
+
+export const registerUser = (userData, history) => dispatch => {
   axios
     .post('/api/users/register', userData)
     .then(user => {
@@ -13,5 +19,31 @@ export const registerUser = (userData, history) => dispatch =>{
         payload: err.response.data,
       });
     });
-
 };
+
+
+export const setCurrentUser = decodedUserData => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decodedUserData,
+  };
+};
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(user => {
+      const { token } = user.data;
+
+      localStorage.setItem('jwtToken', token);
+      setAuthToken(token);
+      const decodedToken = jwt_decode(token);
+      dispatch(setCurrentUser(decodedToken));
+    })
+    .catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -25,7 +25,7 @@ class Login extends Component {
       password: this.state.password,
     };
 
-    console.log('login data', loginUser, e);
+
   };
 
   render() {
